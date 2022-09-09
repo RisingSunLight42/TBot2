d823908a75e8afd69148da89b9cd0dfe486711f1
RisingSunLight <risingsunlight5776@gmail.com>
2022-09-09 23:31:13 +0200

    🏷️ add Data type

---
+++ src/helpers/types/data.ts
@@ -0,0 +1,12 @@
+export interface Data {
+    jour: string;
+    mois: string;
+    annee: string;
+    cours: string;
+    typeCours: string;
+    groupe: string;
+    salle: string;
+    enseignant: string;
+    hDebut: string;
+    hFin: string;
+}
