6cbe251d007c07f65b5f3b93620fce6918290991
RisingSunLight <risingsunlight5776@gmail.com>
2022-09-09 23:31:42 +0200

    🔨 Add return type

--- src/helpers/functions/fetchEdt.ts
+++ src/helpers/functions/fetchEdt.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { Data } from "../types/data";
 
-export const fetchEdt = async (classe: number) => {
+export const fetchEdt = async (classe: number): Promise<Data[]> => {
     const edt = await axios.get(
         `https://zimmermanna.users.greyc.fr/edt/php/EDTReader.php?ressource=${classe}&format=week`
     );
