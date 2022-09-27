b50745c7b74be260c682118b702c76b22d1c137a
RisingSunLight <risingsunlight5776@gmail.com>
2022-09-27 22:43:25 +0200

    🐛 Fix d'un bug où certaines données sont vides, faisant crash le bot

--- src/commands/seeEdt.ts
+++ src/commands/seeEdt.ts
@@ -120,25 +120,27 @@ module.exports = {
 
         const arrEmbed = [];
         for (const jourData of edtDataAsked) {
-            const numJour = parseInt(jourData[0].jour);
-            const numMonth = jourData[0].mois;
-            const year = jourData[0].annee;
-            const day = new Date(
-                `${year}-${numMonth}-${numJour} 12:00:00`
-            ).getDay();
-            const arrFields = [];
-            for (const heureData of jourData) {
-                arrFields.push({
-                    name: `${heureData.hDebut} - ${heureData.hFin}`,
-                    value: `${heureData.cours}\n${heureData.enseignant}\nSalle : ${heureData.salle}`,
-                });
+            if (jourData.length != 0) {
+                const numJour = parseInt(jourData[0].jour);
+                const numMonth = jourData[0].mois;
+                const year = jourData[0].annee;
+                const day = new Date(
+                    `${year}-${numMonth}-${numJour} 12:00:00`
+                ).getDay();
+                const arrFields = [];
+                for (const heureData of jourData) {
+                    arrFields.push({
+                        name: `${heureData.hDebut} - ${heureData.hFin}`,
+                        value: `${heureData.cours}\n${heureData.enseignant}\nSalle : ${heureData.salle}`,
+                    });
+                }
+                arrEmbed.push(
+                    embedGenerator({
+                        title: `Emploi du Temps du ${staticDay[day]} ${numJour} ${staticMonth[numMonth]} ${year}`,
+                        fields: arrFields,
+                    })
+                );
             }
-            arrEmbed.push(
-                embedGenerator({
-                    title: `Emploi du Temps du ${staticDay[day]} ${numJour} ${staticMonth[numMonth]} ${year}`,
-                    fields: arrFields,
-                })
-            );
         }
 
         await interaction.reply({
