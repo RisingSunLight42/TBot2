c24bca086de9a075c6a61383bd099bcce4ad106f
RisingSunLight <risingsunlight5776@gmail.com>
2022-09-27 22:12:39 +0200

    ✨ Ajout edt salles, resolves #21

--- src/commands/seeEdt.ts
+++ src/commands/seeEdt.ts
@@ -14,46 +14,108 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("edt")
         .setDescription("Permet d'avoir l'edt")
-        .addNumberOption((option) =>
-            option
+        .addSubcommand((subcommand) =>
+            subcommand
                 .setName("classe")
-                .setDescription("La classe dont tu veux l'emploi du temps")
-                .addChoices(
-                    { name: "TP2.1", value: 1185 },
-                    { name: "TP2.2", value: 1186 }
+                .setDescription("Permet d'avoir l'EdT de la classe.")
+                .addNumberOption((option) =>
+                    option
+                        .setName("classe")
+                        .setDescription(
+                            "La classe dont tu veux l'emploi du temps"
+                        )
+                        .addChoices(
+                            { name: "TP2.1", value: 1185 },
+                            { name: "TP2.2", value: 1186 }
+                        )
+                        .setRequired(true)
                 )
-                .setRequired(true)
+                .addNumberOption((option) =>
+                    option
+                        .setName("jour")
+                        .setDescription(
+                            "La semaine ne prend pas en compte les W-E, donc aujourd'hui en weed-end donnera Lundi !"
+                        )
+                        .addChoices(
+                            { name: "Aujourd'hui", value: 0 },
+                            { name: "+ 1 jour", value: 1 },
+                            { name: "+ 2 jours", value: 2 },
+                            { name: "+ 3 jours", value: 3 },
+                            { name: "+ 4 jours", value: 4 },
+                            { name: "+ 5 jours", value: 5 }
+                        )
+                        .setRequired(true)
+                )
+                .addBooleanOption((option) =>
+                    option
+                        .setName("affichage")
+                        .setDescription(
+                            "Dois-je afficher les jours intermédiaires ou pas ?"
+                        )
+                        .setRequired(true)
+                )
         )
-        .addNumberOption((option) =>
-            option
-                .setName("jour")
-                .setDescription(
-                    "La semaine ne prend pas en compte les W-E, donc aujourd'hui en weed-end donnera Lundi !"
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("salle")
+                .setDescription("Permet d'avoir l'EdT d'une salle.")
+                .addNumberOption((option) =>
+                    option
+                        .setName("salle")
+                        .setDescription(
+                            "La salle dont tu veux l'emploi du temps"
+                        )
+                        .addChoices(
+                            { name: "1106", value: 38541 },
+                            { name: "1109", value: 37355 },
+                            { name: "1110", value: 39548 },
+                            { name: "2124", value: 38756 },
+                            { name: "2127", value: 39491 },
+                            { name: "2129", value: 38113 },
+                            { name: "2234", value: 39052 },
+                            { name: "2235", value: 37590 },
+                            { name: "2236", value: 38484 },
+                            { name: "2237", value: 39005 },
+                            { name: "2240", value: 140414 },
+                            { name: "Labo de langues", value: 38713 },
+                            { name: "Labo multimédia", value: 24132 }
+                        )
+                        .setRequired(true)
                 )
-                .addChoices(
-                    { name: "Aujourd'hui", value: 0 },
-                    { name: "+ 1 jour", value: 1 },
-                    { name: "+ 2 jours", value: 2 },
-                    { name: "+ 3 jours", value: 3 },
-                    { name: "+ 4 jours", value: 4 },
-                    { name: "+ 5 jours", value: 5 }
+                .addNumberOption((option) =>
+                    option
+                        .setName("jour")
+                        .setDescription(
+                            "La semaine ne prend pas en compte les W-E, donc aujourd'hui en weed-end donnera Lundi !"
+                        )
+                        .addChoices(
+                            { name: "Aujourd'hui", value: 0 },
+                            { name: "+ 1 jour", value: 1 },
+                            { name: "+ 2 jours", value: 2 },
+                            { name: "+ 3 jours", value: 3 },
+                            { name: "+ 4 jours", value: 4 },
+                            { name: "+ 5 jours", value: 5 }
+                        )
+                        .setRequired(true)
                 )
-                .setRequired(true)
-        )
-        .addBooleanOption((option) =>
-            option
-                .setName("affichage")
-                .setDescription(
-                    "Dois-je afficher les jours intermédiaires ou pas ?"
+                .addBooleanOption((option) =>
+                    option
+                        .setName("affichage")
+                        .setDescription(
+                            "Dois-je afficher les jours intermédiaires ou pas ?"
+                        )
+                        .setRequired(true)
                 )
-                .setRequired(true)
         ),
-
     async execute(interaction: ChatInputCommandInteraction) {
-        const classe = interaction.options.getNumber("classe", true);
-        const jour = interaction.options.getNumber("jour", true);
-        const affichage = interaction.options.getBoolean("affichage", true);
-        const edtData = await fetchEdt(classe);
+        const opt = interaction.options;
+        const infoEdt =
+            opt.getSubcommand() === "classe"
+                ? opt.getNumber("classe", true)
+                : opt.getNumber("salle", true);
+        const jour = opt.getNumber("jour", true);
+        const affichage = opt.getBoolean("affichage", true);
+        const edtData = await fetchEdt(infoEdt);
         const edtDataAsked = await dataEdtProcessing(edtData, jour, affichage);
 
         const arrEmbed = [];
@@ -80,7 +142,7 @@ module.exports = {
         }
 
         await interaction.reply({
-            content: "Voici l'emploi du temps demandé !",
+            content: `Voici l'emploi du temps demandé pour la ${opt.getSubcommand()} demandée !`,
             embeds: arrEmbed,
             ephemeral:
                 interaction.channel?.type === ChannelType.DM ||
