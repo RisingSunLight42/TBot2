7284125617a90632f7f84e3f8b89b4f15c5895d2
Ricozzi Romain <risingsunlightdev@gmail.com>
2024-01-24 21:08:34 +0100

    fix: change edt link since the old one is deprecated

--- src/helpers/functions/fetchEdt.ts
+++ src/helpers/functions/fetchEdt.ts
@@ -3,7 +3,7 @@ import { Data } from "../types/data";
 
 export const fetchEdt = async (classe: number): Promise<Data[]> => {
     const edt = await axios.get(
-        `https://www.stlo.unicaen.fr/edt/php/EDTReader.php?ressource=${classe}&format=week`,
+        `https://zimmermanna.users.greyc.fr/edt/php/EDTReader.php?ressource=${classe}&format=week`,
     );
     return edt.data;
 };
